9586f0d671141c2a3c8bd3e02f55c0b670f04f65
h-khribich <hamza.khribich@gmail.com>
2021-07-30 01:47:24 +0200

    Photographer page banner implemented

--- photographer-page.js
+++ photographer-page.js
@@ -5,11 +5,14 @@ const photographerName = document.querySelector('.name-block__name');
 const photographerLocation = document.querySelector('.name-block__location');
 const photographerTagline = document.querySelector('.name-block__tagline');
 const photographerTagList = document.querySelector('.name-block__taglist');
+const photographerImage = document.querySelector('.photographer__img');
 
 function fillPhotographerBanner(element) {
   photographerName.innerText = element.name;
   photographerLocation.innerText = `${element.city}, ${element.country}`;
   photographerTagline.innerText = element.tagline;
+  photographerImage.setAttribute('src', `assets/Photographers ID Photos/${element.portrait}`);
+  photographerImage.alt = element.name;
 
   element.tags.forEach((tag) => {
     const photographerTag = photographerTagList.appendChild(document.createElement('li'));
@@ -31,7 +34,6 @@ fetch('fisheye_data.json')
       if (photographer.id === pageID) {
         fillPhotographerBanner(photographer);
       }
-
     });
   })
   .catch((err) => (err));
