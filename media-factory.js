d26e9a0597b292bbb44a741be9f48c8da341b78b
h-khribich <hamza.khribich@gmail.com>
2021-08-09 00:48:24 +0200

    Factory function implemented for media gallery

--- media-factory.js
+++ media-factory.js
@@ -1,13 +1,46 @@
 /* -- General & DOM Selectors -- */
-const pageId = localStorage.getItem('pageId');
-
+const pageId = parseInt(localStorage.getItem('pageId'), 10);
+const mediaGallery = document.getElementById('media-gallery');
 const photographerMedia = [];
 
 // Media factory function
-function mediaFactory(type) {
-  if (type === 'video') {
-    console.log(type);
-  }
+function mediaFactory(element) {
+  return {
+    createMedia: function createMedia() {
+      const mediaContainer = document.createElement('div');
+      mediaContainer.classList.add('media__container');
+      // Depending on the type of media, a different HTML element is created
+      if (element.video) {
+        const placeholder = document.createElement('p');
+        placeholder.innerText = 'video placeholder';
+        mediaContainer.appendChild(placeholder);
+      }
+      if (element.image) {
+        const mediaImage = mediaContainer.appendChild(document.createElement('img'));
+        mediaImage.setAttribute('src', `assets/${element.photographerId}/${element.image}`);
+        mediaImage.setAttribute('alt', `${element['alt-text']}`);
+        mediaImage.classList.add('media', 'media__image');
+        // Append element to container
+        mediaContainer.appendChild(mediaImage);
+      }
+      // Media description container, title and likes
+      const mediaDescription = mediaContainer.appendChild(document.createElement('div'));
+      mediaDescription.classList.add('media__description');
+
+      const mediaTitle = document.createElement('p');
+      mediaTitle.innerText = `${element.title}`;
+      mediaTitle.classList.add('media__title');
+
+      const mediaLikes = document.createElement('span');
+      mediaLikes.innerText = `${element.likes}`;
+      mediaLikes.classList.add('media__likes');
+
+      mediaDescription.append(mediaTitle, mediaLikes);
+
+      // Add the filled object to the HTML body
+      mediaGallery.appendChild(mediaContainer);
+    },
+  };
 }
 
 fetch('fisheye_data.json')
@@ -21,16 +54,18 @@ fetch('fisheye_data.json')
     });
 
     photographerMedia.forEach((media) => {
-      // Accessing type of media through key
-      let mediaType = '';
-      Object.keys(media).forEach((key) => {
-        if ((key === 'image') || (key === 'video')) {
-          mediaType = key;
-        }
-      });
-
-      // Calling factory function with media type as a parameter
-      mediaFactory(mediaType);
+      const newMedia = mediaFactory(media);
+      newMedia.createMedia();
     });
   })
   .catch((err) => err);
+
+// Accessing type of media through key
+/*
+let mediaType = '';
+Object.keys(media).forEach((key) => {
+  if ((key === 'image') || (key === 'video')) {
+    mediaType = key;
+  }
+});
+*/
